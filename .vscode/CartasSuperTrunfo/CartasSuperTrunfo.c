c54555ed6b0602a11c0a808a302f76dd7bd8962b
Icemen1237 <Ice-men-1237@outlook.com.br>
2025-03-16 00:18:10 +0000

    foi adicionada variáveis de estado, codigo, cidade populção, área, Pib e número de pontos turístico.

---
+++ .vscode/CartasSuperTrunfo/CartasSuperTrunfo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main() {
+    
+char  estado;
+char  codigo[4];
+char  cidade[20];
+int   população;
+int   ponto_turistico;
+float área, PIB;
+
+
+printf("Digite o estado: \n");
+scanf("%s", estado);
+
+printf("Digite o codigo da carta: \n");
+scanf("%s", codigo);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", cidade);
+
+print("Digite a população: \n");
+scanf("%d", &população);
+
+printf("Digite a área: \n");
+scanf("%f", &área);
+
+print("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite o numero de pontos turisticos: \n");
+scanf("%d", &ponto_turistico);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
