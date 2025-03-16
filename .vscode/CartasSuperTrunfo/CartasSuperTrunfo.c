64e0251e5ce0d0c443bae3f847f6c338f8fb8c3f
Icemen1237 <Ice-men-1237@outlook.com.br>
2025-03-16 02:30:44 +0000

    foram adicionados comtários

--- .vscode/CartasSuperTrunfo/CartasSuperTrunfo.c
+++ .vscode/CartasSuperTrunfo/CartasSuperTrunfo.c
@@ -1,53 +1,82 @@
 #include <stdio.h>
 int main() {
     
-char  estado;
+char  estado[20];
 char  codigo[4];
 char  cidade[20];
-int   população;
-int   ponto_turistico;
-float área, PIB;
+int   populacao, ponto_turistico;
+float Area, PIB;
 
 
-printf("Digite o estado: \n");
-scanf("%s", estado);
+printf("Digite o estado: "); // O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", estado); // aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
-printf("Digite o codigo da carta: \n");
-scanf("%s", codigo);
 
-printf("Digite o nome da cidade: \n");
-scanf("%s", cidade);
-
-print("Digite a população: \n");
-scanf("%d", &população);
-
-printf("Digite a área: \n");
-scanf("%f", &área);
-
-print("Digite o PIB: \n");
-scanf("%f", &PIB);
-
-printf("Digite o numero de pontos turisticos: \n");
-scanf("%d", &ponto_turistico);
+printf("Digite o código da carta: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", codigo);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
 
 
+printf("Digite o nome da cidade: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", cidade);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
 
 
+printf("Digite a população: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%d", &populacao);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
 
 
+printf("Digite a área: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%f", &Area);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
 
 
+printf("Digite o PIB: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%f", &PIB);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
 
 
 
+printf("Digite o número de pontos turísticos: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%d\n", &ponto_turistico);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado.
 
 
 
+// Próximos dados solicitados
 
+
+
+printf("Digite o estado: "); // O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", estado); // aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+printf("Digite o código da carta: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", codigo);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+
+printf("Digite o nome da cidade: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%s", cidade);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+
+printf("Digite a população: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%d", &populacao);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+
+printf("Digite a área: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%f", &Area);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+
+printf("Digite o PIB: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%f", &PIB);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado. 
+
+
+
+printf("Digite o número de pontos turísticos: ");// O programa vai imprimir uma mensagem sobre qual dado o usuário deve inserir;
+scanf("%d", &ponto_turistico);// aqui aparecerá um prompt onde o usuário deve inserir o dado solicitado.
 
 
 
